162f6dd3f2f15906917f884e4df8077b8c3d219a
andreykribs <andreykribs@mail.ru>
2024-09-09 23:38:38 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,8 @@
+n = int(input("Введите число: "))
+result = ":"
+for i in range(1, n):
+    for j in range(i + 1, n):
+        if n % (i + j) == 0:
+            result += str(i) + str(j)
+            continue
+print(result)
